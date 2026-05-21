21d352e95fcfc4edb0ccc9d567adcd6c51592947
anna-grim <anna.grim@alleninstitute.org>
2026-05-21 19:29:33 +0000

    feat: swc loading, graph build, graph class, viz

---
+++ src/agentic_neuron_proofreader/utils/graph_util.py
@@ -0,0 +1,425 @@
+"""
+Created on Thu May 21 12:00:00 2026
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+Code that loads and preprocesses neuron fragments stored as SWC files. The
+graph loader object called inside our custom graph classes.
+
+"""
+
+from collections import deque
+from concurrent.futures import (
+    FIRST_COMPLETED,
+    ProcessPoolExecutor,
+    wait,
+)
+from scipy.spatial.distance import euclidean
+from tqdm import tqdm
+
+import networkx as nx
+import numpy as np
+
+from neuron_proofreader.utils import geometry_util as geometry, swc_util, util
+
+
+class GraphLoader:
+    """
+    Class that loads SWC files and constructs a FragmentsGraph instance from
+    the data.
+    """
+
+    def __init__(
+        self,
+        anisotropy=(1.0, 1.0, 1.0),
+        min_cable_length=40.0,
+        node_spacing=1,
+        prefetch=128,
+        prune_depth=24.0,
+        verbose=False,
+    ):
+        """
+        Builds a FragmentsGraph by reading swc files stored on either the
+        cloud or local machine, then extracting the irreducible components.
+
+        Parameters
+        ----------
+        anisotropy : Tuple[float], optional
+            Image to physical coordinates scaling factors to account for the
+            anisotropy of the microscope. Default is (1.0, 1.0, 1.0).
+        min_cable_length : float, optional
+            Minimum cable length (in microns) of SWC files that are loaded.
+            Default is 40.
+        node_spacing : int, optional
+            Spacing (in microns) between neighboring nodes. Default is 1.
+        prefetch : int, optional
+            Number of jobs to prefetch. Default is 128.
+        prune_depth : int, optional
+            Branches with length less than "prune_depth" microns are pruned.
+            Default is 24.
+        verbose : bool, optional
+            Indication of whether to display a progress bar while building
+            FragmentsGraph. Default is True.
+        """
+        # Instance attributes
+        self.min_cable_length = min_cable_length
+        self.node_spacing = node_spacing
+        self.prefetch = prefetch
+        self.prune_depth = prune_depth
+        self.swc_reader = swc_util.Reader(anisotropy, min_cable_length, verbose)
+        self.verbose = verbose
+
+    def __call__(self, swc_pointer):
+        """
+        Processes a list of SWC dictionaries in parallel and loads the
+        components of the irreducible subgraph from each.
+
+        Parameters
+        ----------
+        swc_pointer : str
+            Path to SWC files to be read.
+
+        Returns
+        -------
+        irreducibles : List[dict]
+            Dictionaries containing components of the irreducible subgraph
+            loaded from SWC files.
+        """
+        # Read SWC files
+        swc_dicts = self.swc_reader(swc_pointer)
+        if self.verbose:
+            pbar = tqdm(total=len(swc_dicts), desc="Load Graphs")
+
+        # Load graphs
+        with ProcessPoolExecutor() as executor:
+            # Start processes
+            pending = set()
+            while len(pending) < self.prefetch and swc_dicts:
+                pending.add(executor.submit(self.load, swc_dicts.pop()))
+
+            # Yield processes
+            irreducibles = deque()
+            while pending:
+                done, pending = wait(pending, return_when=FIRST_COMPLETED)
+                for future in done:
+                    # Store completed processes
+                    result = future.result()
+                    if result:
+                        irreducibles.append(result)
+                    pbar.update(1) if self.verbose else None
+
+                    # Continue submitting processes
+                    if swc_dicts:
+                        pending.add(
+                            executor.submit(self.load, swc_dicts.pop())
+                        )
+        return irreducibles
+
+    def load(self, swc_dict):
+        """
+        Loads irreducible components from "swc_dict", which is assumed to
+        contain exactly one connected component.
+
+        Parameters
+        ----------
+        swc_dict : dict
+            Contents of an SWC file.
+
+        Returns
+        -------
+        dict
+            Dictionary that each contains the components of an irreducible
+            subgraph.
+        """
+        # Build graph
+        graph = swc_util.to_graph(swc_dict, set_attrs=True)
+        prune_short_branches(graph, self.prune_depth)
+
+        # Extract irreducible components (if applicable)
+        if self.satisfies_cable_length_condition(graph):
+            irreducibles = self.get_irreducibles(graph)
+            if irreducibles:
+                irreducibles["is_soma"] = len(swc_dict["soma_nodes"]) > 0
+                irreducibles["swc_id"] = swc_dict["swc_name"]
+            return irreducibles
+        else:
+            return None
+
+    def get_irreducibles(self, graph):
+        """
+        Identifies irreducible components of a connected graph.
+
+        Parameters
+        ----------
+        graph : networkx.Graph
+            Graph to be searched.
+
+        Returns
+        -------
+        irreducibles : dict
+            Dictionary containing the irreducible components of a connected
+            graph.
+        """
+
+        def dist(i, j):
+            """
+            Computes distance between the given nodes.
+
+            Parameters
+            ----------
+            i : int
+                Node ID.
+            j : int
+                Node ID.
+
+            Returns
+            -------
+            float
+                Distance between nodes.
+            """
+            return euclidean(graph.graph["xyz"][i], graph.graph["xyz"][j])
+
+        # Initializations
+        leaf = find_leaf(graph)
+        irreducible_nodes = {leaf}
+        irreducible_edges = dict()
+
+        # Main
+        root, cable_length = None, 0
+        for i, j in nx.dfs_edges(graph, source=leaf):
+            # Check for start of irreducible edge
+            if root is None:
+                root, edge_length = i, 0
+                attrs = {
+                    "radius": [graph.graph["radius"][i]],
+                    "xyz": [graph.graph["xyz"][i]],
+                }
+
+            # Visit node
+            edge_length += dist(i, j)
+            attrs["radius"].append(graph.graph["radius"][j])
+            attrs["xyz"].append(graph.graph["xyz"][j])
+
+            # Check for end of irreducible edge
+            if graph.degree[j] != 2:
+                cable_length += edge_length
+                irreducible_nodes.add(j)
+
+                attrs = to_numpy(attrs)
+                n_pts = int(edge_length / self.node_spacing)
+                self.resample_curve_3d(graph, attrs, (root, j), n_pts)
+
+                irreducible_edges[(root, j)] = attrs
+                root = None
+
+        # Check for curvy line fragment
+        if len(irreducible_nodes) == 2:
+            endpoint_dist = dist(*tuple(irreducible_nodes))
+            if endpoint_dist / cable_length < 0.5:
+                return None
+
+        # Store results
+        if cable_length > self.min_cable_length:
+            irreducibles = {
+                "nodes": set_node_attrs(graph, irreducible_nodes),
+                "edges": set_edge_attrs(graph, irreducible_edges),
+            }
+        else:
+            irreducibles = None
+        return irreducibles
+
+    # --- Helpers ---
+    def satisfies_cable_length_condition(self, graph):
+        """
+        Determines whether the cable length of the given graph is greater
+        than "self.min_cable_length".
+
+        Parameters
+        ----------
+        graph : networkx.Graph
+            Graph to be checked.
+
+        Returns
+        -------
+        bool
+            Indication of whether the total cable length of the given graph is
+            greater than "self.min_cable_length".
+        """
+        length = 0
+        for i, j in nx.dfs_edges(graph):
+            length += euclidean(graph.graph["xyz"][i], graph.graph["xyz"][j])
+            if length > self.min_cable_length:
+                return True
+        return False
+
+    def resample_curve_3d(self, graph, attrs, edge, n_pts):
+        """
+        Smooths a 3D curve and update the corresponding edge endpoints in the
+        graph.
+
+        Parameters
+        ----------
+        graph : networkx.Graph
+            Graph to be updated.
+        attrs : dict
+            Dictionary containing "xyz" (list of 3D points) and "radius" (list
+            of scalars) representing the edge to be smoothed.
+        edge : Tuple[int]
+            Start and end node IDs of the edge.
+        n_pts : int
+            Number of points to use for the smoothed curve.
+        """
+        attrs["xyz"] = geometry.resample_curve_3d(attrs["xyz"], n_pts=n_pts)
+        attrs["radius"] = geometry.resample_curve_1d(attrs["radius"], n_pts)
+        graph.graph["xyz"][edge[0]] = attrs["xyz"][0]
+        graph.graph["xyz"][edge[1]] = attrs["xyz"][-1]
+
+
+# --- Helpers ---
+def set_node_attrs(graph, nodes):
+    """
+    Extracts attributes for each node in the graph.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph that contains "nodes".
+    nodes : List[int]
+        Nodes whose attributes are to be extracted from the graph.
+
+    Returns
+    -------
+    attrs : dict
+        Dictionary where the keys are node ids and values are dictionaries
+        containing the "radius" and "xyz" attributes of the nodes.
+    """
+    attrs = dict()
+    for i in nodes:
+        attrs[i] = {
+            "radius": graph.graph["radius"][i],
+            "xyz": graph.graph["xyz"][i],
+        }
+    return attrs
+
+
+def set_edge_attrs(graph, attrs):
+    """
+    Sets the edge attributes of a given graph by updating node coordinates and
+    resamples points in irreducible path.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph that attributes dictionary was built from.
+    attrs : dict
+        Dictionary where the keys are irreducible edge IDs and values are the
+        corresponding attribute dictionaries.
+
+    Returns
+    -------
+    attrs : dict
+        Updated edge attribute dictionary.
+    """
+    for e in attrs:
+        i, j = tuple(e)
+        attrs[e]["xyz"][0] = graph.graph["xyz"][i]
+        attrs[e]["xyz"][-1] = graph.graph["xyz"][j]
+    return attrs
+
+
+# --- Miscellaneous ---
+def count_nodes(irreducibles):
+    n = 0
+    for irr in irreducibles:
+        n += len(irr["nodes"])
+        for attrs in irr["edges"].values():
+            n += len(attrs["xyz"]) - 2
+    return n
+
+
+def cycle_exists(graph):
+    """
+    Checks if the given graph has a cycle.
+
+    Paramaters
+    ----------
+    graph : networkx.Graph
+        Graph to be searched.
+
+    Returns
+    -------
+    bool
+        Indication of whether graph has a cycle.
+    """
+    try:
+        nx.find_cycle(graph)
+        return True
+    except nx.exception.NetworkXNoCycle:
+        return False
+
+
+def find_leaf(graph):
+    """
+    Finds a leaf node in the given graph.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be searched.
+
+    Returns
+    -------
+    i : int
+        Leaf node.
+    """
+    for i in graph.nodes:
+        if graph.degree[i] == 1:
+            return i
+
+
+def prune_short_branches(graph, depth):
+    """
+    Prunes branches with length less than "depth" microns.
+
+    Parameters
+    ----------
+    graph : networkx.Graph
+        Graph to be searched.
+    depth : float
+        Length of branches that are pruned.
+    """
+    for leaf in [i for i in graph.nodes if graph.degree[i] == 1]:
+        branch = [leaf]
+        length = 0
+        for i, j in nx.dfs_edges(graph, source=leaf):
+            # Visit edge
+            length += euclidean(graph.graph["xyz"][i], graph.graph["xyz"][j])
+            if length > depth:
+                break
+
+            # Check whether to continue search
+            if graph.degree(j) == 2:
+                branch.append(j)
+            elif graph.degree(j) > 2:
+                graph.remove_nodes_from(branch)
+                break
+
+
+def to_numpy(attrs):
+    """
+    Converts edge attributes from a list to NumPy array.
+
+    Parameters
+    ----------
+    attrs : dict
+        Edge attribute dictionary.
+
+    Returns
+    -------
+    attrs : dict
+        Updated edge attribute dictionary.
+    """
+    attrs["xyz"] = np.array(attrs["xyz"], dtype=np.float32)
+    attrs["radius"] = np.array(attrs["radius"], dtype=np.float16)
+    return attrs
---
+++ src/agentic_neuron_proofreader/utils/util.py
@@ -0,0 +1,425 @@
+"""
+Created on Thu May 21 12:00:00 2026
+
+@author: Anna Grim
+@email: anna.grim@alleninstitute.org
+
+General helper routines.
+
+"""
+
+from botocore import UNSIGNED
+from botocore.client import Config
+from collections import deque
+from random import sample
+from google.cloud import storage
+from io import BytesIO, StringIO
+from zipfile import ZipFile
+
+import boto3
+import os
+import pandas as pd
+import shutil
+
+
+# -- OS Utils ---
+def list_dir(dir_path, extension=""):
+    """
+    Lists filenames in the given directory. If "extension" is provided,
+    filenames ending with the given extension are returned.
+
+    Parameters
+    ----------
+    dir_path : str
+        Path to directory to be searched.
+    extension : str, optional
+       Extension of filenames to be returned. Default is an empty string.
+
+    Returns
+    -------
+    List[str]
+        Filenames in the given directory.
+    """
+    return [f for f in os.listdir(dir_path) if f.endswith(extension)]
+
+
+def list_files_in_zip(zip_content):
+    """
+    Lists all files in a ZIP archive stored in a GCS bucket.
+
+    Parameters
+    ----------
+    zip_content : str
+        Content stored in a ZIP archive in the form of a string of bytes.
+
+    Returns
+    -------
+    List[str]
+        Filenames in a ZIP archive file.
+    """
+    with ZipFile(BytesIO(zip_content), "r") as zip_file:
+        return zip_file.namelist()
+
+
+def list_paths(dir_path, extension=None):
+    """
+    Lists paths of files in the given directory. If "extension" is provided,
+    filenames ending with the given extension are returned.
+
+    Parameters
+    ----------
+    dir_path : str
+        Path to directory to be searched.
+    extension : str, optional
+        Extension of filenames to be returned. Default is None.
+
+    Returns
+    -------
+    paths : List[str]
+        Paths of files in the given directory.
+    """
+    paths = list()
+    for f in list_dir(dir_path, extension=extension):
+        paths.append(os.path.join(dir_path, f))
+    return paths
+
+
+def mkdir(dir_path, delete=False):
+    """
+    Creates a directory at the given path.
+
+    Parameters
+    ----------
+    dir_path : str
+        Path of directory to be created.
+    delete : bool, optional
+        Indication of whether to delete the directory if it already exists
+        Default is False.
+    """
+    if delete:
+        rmdir(dir_path)
+
+    os.makedirs(dir_path, exist_ok=True)
+
+
+def rmdir(dir_path):
+    """
+    Removes the given directory and all of its subdirectories.
+
+    Parameters
+    ----------
+    dir_path : str
+        Path to directory to be removed if it exists.
+    """
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+
+
+def rm_file(path):
+    """
+    Removes the file at the given path.
+
+    Parameters
+    ----------
+    path : str
+        Path to file to be removed.
+    """
+    if os.path.exists(path):
+        os.remove(path)
+
+
+# --- IO Utils ---
+def read_json(path):
+    """
+    Reads JSON file located at the given path.
+
+    Parameters
+    ----------
+    path : str
+        Path to JSON file to be read.
+
+    Returns
+    -------
+    dict
+        Contents of JSON file.
+    """
+    return pd.read_json(path, storage_options={"anon": True}, typ="series")
+
+
+def read_txt(path):
+    """
+    Reads txt file at the given path.
+
+    Parameters
+    ----------
+    path : str
+        Path to txt file.
+
+    Returns
+    -------
+    str
+        Text from the txt file.
+    """
+    if is_s3_path(path):
+        return read_txt_from_s3(path)
+    elif is_gcs_path(path):
+        return read_txt_from_gcs(path)
+    else:
+        with open(path, "r") as f:
+            return f.read()
+
+
+def read_zip(zip_file, path):
+    """
+    Reads txt file contained in the given ZIP archive.
+
+    Parameters
+    ----------
+    zip_file : ZipFile
+        ZIP archive containing TXT file.
+
+    Returns
+    -------
+    str
+        Contents of a TXT file.
+    """
+    with zip_file.open(path) as f:
+        return f.read().decode("utf-8")
+
+
+# --- Cloud Utils ---
+def parse_cloud_path(path):
+    """
+    Parses a cloud storage path into its bucket name and prefix. Supports
+    paths of the form: "{scheme}://bucket_name/prefix" or without a scheme.
+
+    Parameters
+    ----------
+    path : str
+        Path to be parsed.
+
+    Returns
+    -------
+    bucket_name : str
+        Name of the bucket.
+    prefix : str
+        Cloud prefix.
+    """
+    # Split path
+    path = path[len("s3://"):] if is_s3_path else path[len("gs://"):]
+    parts = path.split("/", 1)
+
+    # Extract bucket and prefix
+    bucket_name = parts[0]
+    prefix = parts[1] if len(parts) > 1 else ""
+    return bucket_name, prefix
+
+
+def list_cloud_paths(path, extension=""):
+    """
+    Lists all files in a GCS/S3 bucket with the given extension.
+
+    Parameters
+    ----------
+    path : str
+        Path to cloud prefix to be searched, must be in the format:
+        f"{scheme}://{bucket_name}/{prefix}".
+    extension : str, optional
+        File extension of filenames to be listed. Default is an empty string.
+
+    Returns
+    -------
+    List[str]
+        Filenames stored at the GCS path with the given extension.
+    """
+    assert is_gcs_path(path) or is_s3_path(path)
+    bucket_name, prefix = parse_cloud_path(path)
+    list_fn = list_gcs_paths if is_gcs_path(path) else list_s3_paths
+    return list_fn(bucket_name, prefix, extension=extension)
+
+
+# -- GCS Utils --
+def is_gcs_path(path):
+    """
+    Checks if the path is a GCS path.
+
+    Parameters
+    ----------
+    path : str
+        Path to be checked.
+
+    Returns
+    -------
+    bool
+        Indication of whether the path is a GCS path.
+    """
+    return path.startswith("gs://")
+
+
+def list_gcs_paths(bucket_name, prefix, extension=""):
+    """
+    Lists paths at a GCS prefix with the given extension.
+
+    Parameters
+    ----------
+    bucket_name : str
+        Name of bucket containing prefix.
+    prefix : str
+        Path to location within bucket to be searched.
+    extension : str, optional
+        File extension of filenames to be listed. Default is an empty string.
+
+    Returns
+    -------
+    List[str]
+        Paths under the GCS prefix with the given extension.
+    """
+    bucket = storage.Client().bucket(bucket_name)
+    paths = list()
+    for name in [b.name for b in bucket.list_blobs(prefix=prefix)]:
+        if extension in name:
+            paths.append(os.path.join(f"gs://{bucket_name}", name))
+    return paths
+
+
+def list_gcs_subdirectories(bucket_name, prefix):
+    """
+    Lists all direct subdirectories of a given prefix in a GCS bucket.
+
+    Parameters
+    ----------
+    bucket : str
+        Name of bucket to be read from.
+    prefix : str
+        Path to directory in the specified bucket.
+
+    Returns
+    -------
+    subdirs : List[str]
+         Direct subdirectories.
+    """
+    # Load blobs
+    storage_client = storage.Client()
+    blobs = storage_client.list_blobs(
+        bucket_name, prefix=prefix, delimiter="/"
+    )
+    [blob.name for blob in blobs]
+
+    # Parse directory contents
+    prefix_depth = len(prefix.split("/"))
+    subdirs = list()
+    for prefix in blobs.prefixes:
+        is_dir = prefix.endswith("/")
+        is_direct_subdir = len(prefix.split("/")) - 1 == prefix_depth
+        if is_dir and is_direct_subdir:
+            subdirs.append(prefix)
+    return subdirs
+
+
+def read_txt_from_gcs(path):
+    """
+    Reads a txt file stored in a GCS bucket.
+
+    Parameters
+    ----------
+    path : str
+        Path to txt file to be read.
+
+    Returns
+    -------
+    str
+        Contents of txt file.
+    """
+    bucket_name, subpath = parse_cloud_path(path)
+    bucket = storage.Client().bucket(bucket_name)
+    return bucket.blob(subpath).download_as_text()
+
+
+# --- S3 Utils ---
+def is_s3_path(path):
+    """
+    Checks if the given path is an S3 path.
+
+    Parameters
+    ----------
+    path : str
+        Path to be checked.
+
+    Returns
+    -------
+    bool
+        Indication of whether the path is an S3 path.
+    """
+    return path.startswith("s3://")
+
+
+def list_s3_paths(bucket_name, prefix, extension=""):
+    """
+    Lists all object keys in a public S3 bucket under a given prefix,
+    optionally filters by file extension.
+
+    Parameters
+    ----------
+    bucket_name : str
+        Name of the S3 bucket.
+    prefix : str
+        Prefix to search under.
+    extension : str, optional
+        File extension to filter by. Default is an empty string.
+
+    Returns
+    -------
+    paths : List[str]
+        S3 object keys that match the prefix and extension filter.
+    """
+    # Create an anonymous client for public buckets
+    s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+
+    # List all objects under the prefix
+    paths = list()
+    if "Contents" in response:
+        for obj in response["Contents"]:
+            filename = obj["Key"]
+            if filename.endswith(extension):
+                path = os.path.join(f"s3://{bucket_name}", filename)
+                paths.append(path)
+    return paths
+
+
+def read_txt_from_s3(path):
+    """
+    Reads a txt file stored in an S3 bucket.
+
+    Parameters
+    ----------
+    path : str
+        Path to txt file to be read.
+
+    Returns
+    -------
+    str
+        Contents of txt file.
+    """
+    bucket_name, subpath = parse_cloud_path(path)
+    s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+    obj = s3.get_object(Bucket=bucket_name, Key=subpath)
+    return obj["Body"].read().decode("utf-8")
+
+
+# --- Miscellaneous ---
+def sample_once(my_container):
+    """
+    Samples a single element from "my_container".
+
+    Parameters
+    ----------
+    my_container : Container
+        Container to be sampled from.
+
+    Returns
+    -------
+    hashable
+        Random element from the given container.
+    """
+    return sample(my_container, 1)[0]
